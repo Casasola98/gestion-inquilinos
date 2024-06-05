4533dc19aa78c770aecebfbe9a68937a4359516e
nicoArley <nikyac13@gmail.com>
2024-06-05 14:37:07 -0600

    General estructure for functionalities

--- server/server.js
+++ server/server.js
@@ -16,6 +16,247 @@ const config = {
 	trustServerCertificate: true
 };
 
+
+app.post('/login', jsonParser, (req, res) => {
+	//ingresarSistema
+})
+
+app.post('/registrar', jsonParser, (req, res) => {
+	//registrarInquilinosPropietarios
+})
+
+app.post('/registrarAdmin', jsonParser, (req, res) => {
+	//registrar solo Admin
+})
+
+//       ********** PROPIETARIO **********
+// MODULO PROPIEDAD (propietario)
+app.post('/crearPropiedad', jsonParser, (req, res) => {
+	
+})
+
+app.post('/visualizarPropiedades', jsonParser, (req, res) => {
+	
+})
+
+app.post('/editarPropiedad', jsonParser, (req, res) => {
+
+})
+
+app.post('/eliminarPropiedad', jsonParser, (req, res) => {
+
+})
+
+// MODULO INQUILINOS (Propietario)
+app.post('/crearInquilino', jsonParser, (req, res) => {
+	//obtiene los datos de la tabla de solicitudes para aceptar y denegar, cambian las solicitudes 
+})
+
+app.post('/visualizarInquilino', jsonParser, (req, res) => {
+	//visualiza inquilinos activos 
+})
+
+app.post('/editarInquilino', jsonParser, (req, res) => {
+	//editar un inquilino  seleccione el botón va pasar a tener la funcionalidad de interrumpir el alquiler.
+})
+
+//Modulo Amenidades (Propietario)
+app.post('/crearAmenidad', jsonParser, (req, res) => {
+	
+})
+
+app.post('/visualizarAmenidad', jsonParser, (req, res) => {
+	
+})
+
+app.post('/editarAmenidad', jsonParser, (req, res) => {
+
+})
+
+app.post('/eliminarAmenidad', jsonParser, (req, res) => {
+
+})
+
+//MODULO MANTENIMIENTO (Propietario)
+app.post('/visualizarMantenimientos', jsonParser, (req, res) => {
+
+})
+
+app.post('/actualizarMantenimientos', jsonParser, (req, res) => {
+
+})
+
+// MODULO DE REPORTES (propietario)
+
+app.post('/reportes', jsonParser, (req, res) => {
+
+})
+
+function definirFechaInicial(periodo) {
+	return periodo;
+}
+
+// MODULO PAGOS (Propietario)
+app.post('/visualizarPagoAdmin', jsonParser, (req, res) => {
+
+})
+
+//MODULO COMUNICACION (Propietario, inquilino)
+app.post('/enviarMensaje', jsonParser, (req, res) => {
+
+})
+
+app.post('/visualizarMsjRecibido', jsonParser, (req, res) => {
+
+})
+app.post('/visualizarMsjEnviados', jsonParser, (req, res) => {
+
+})
+
+//       ********** INQUILINO **********
+// MODULO PAGOS (Inquilino)
+app.post('/registrarPago', jsonParser, (req, res) => {
+
+})
+
+app.post('/visualizarPago', jsonParser, (req, res) => {
+
+})
+
+app.post('/editarPago', jsonParser, (req, res) => {
+
+})
+
+app.post('/eliminarPago', jsonParser, (req, res) => {
+
+})
+//Modulo Alquiler (Inquilino)
+app.post('/solicitarAlquiler', jsonParser, (req, res) => {
+	//En este módulo “Crear” va a ser dos tablas donde salga todos los aquileres disponibles y un boton de solicitar 
+})
+
+app.post('/editarAlquiler', jsonParser, (req, res) => {
+	//se cambian los detalles del alquiler que tenga el usuario 
+})
+
+app.post('/interrumpirAlquiler', jsonParser, (req, res) => {
+	//se va a interrumpir los alquileres en uso o cancelar las solicitudes aún pendientes
+})
+
+// MODULO MANTENIMIENTO(Inquilino)
+app.post('/registrarMantenimiento', jsonParser, (req, res) => {
+	
+})
+
+//       ********** ADMINITRADOR **********
+// MODULO PROPIEDAD (Administrador)
+app.post('/crearPropiedadAdmin', jsonParser, (req, res) => {
+	
+})
+
+app.post('/visualizarPropiedadesAdmin', jsonParser, (req, res) => {
+	
+})
+
+app.post('/editarPropiedadAdmin', jsonParser, (req, res) => {
+
+})
+
+app.post('/eliminarPropiedadAdmin', jsonParser, (req, res) => {
+
+})
+
+// MODULO INQUILINOS (Administrador)
+app.post('/crearInquilinoAdmin', jsonParser, (req, res) => {
+	//obtiene los datos de la tabla de solicitudes para aceptar y denegar, cambian las solicitudes 
+})
+
+app.post('/visualizarInquilinoAdmin', jsonParser, (req, res) => {
+	//visualiza inquilinos activos 
+})
+
+app.post('/editarInquilinoAdmin', jsonParser, (req, res) => {
+	//editar un inquilino  seleccione el botón va pasar a tener la funcionalidad de interrumpir el alquiler.
+})
+
+//Modulo Amenidades (Administrador)
+app.post('/crearAmenidadAdmin', jsonParser, (req, res) => {
+	
+})
+
+app.post('/visualizarAmenidadAdmin', jsonParser, (req, res) => {
+	
+})
+
+app.post('/editarAmenidadAdmin', jsonParser, (req, res) => {
+
+})
+
+app.post('/eliminarAmenidadAdmin', jsonParser, (req, res) => {
+
+})
+
+//MODULO MANTENIMIENTO (Administrador)
+app.post('/visualizarMantenimientosAdmin', jsonParser, (req, res) => {
+
+})
+
+app.post('/actualizarMantenimientosAdmin', jsonParser, (req, res) => {
+
+})
+
+app.post('/registrarMantenimientoAdmin', jsonParser, (req, res) => {
+	
+})
+
+//MODULO REPORTE
+app.post('/reportesAdmin', jsonParser, (req, res) => {
+
+})
+
+//MODULO COMUNICACION (Administrador)
+app.post('/enviarMensajeAdmin', jsonParser, (req, res) => {
+
+})
+
+app.post('/visualizarMsjRecibidoAdmin', jsonParser, (req, res) => {
+
+})
+app.post('/visualizarMsjEnviadosAdmin', jsonParser, (req, res) => {
+
+})
+
+// MODULO PAGOS (Admin)
+app.post('/registrarPagoAdmin', jsonParser, (req, res) => {
+
+})
+
+app.post('/visualizarPagoAdmin', jsonParser, (req, res) => {
+
+})
+
+app.post('/editarPagoAdmin', jsonParser, (req, res) => {
+
+})
+
+app.post('/eliminarPagoAdmin', jsonParser, (req, res) => {
+
+})
+
+//Modulo Alquiler (Admin)
+app.post('/solicitarAlquilerAdmin', jsonParser, (req, res) => {
+	//En este módulo “Crear” va a ser dos tablas donde salga todos los aquileres disponibles y un boton de solicitar 
+})
+
+app.post('/editarAlquilerAdmin', jsonParser, (req, res) => {
+	//se cambian los detalles del alquiler que tenga el usuario 
+})
+
+app.post('/interrumpirAlquilerAdmin', jsonParser, (req, res) => {
+	//se va a interrumpir los alquileres en uso o cancelar las solicitudes aún pendientes
+})
+
+// Los ejemplos
 app.get('/autores', (req, res) => {
 	// Connect to your database
 	mssql.connect(config, function (err) {
