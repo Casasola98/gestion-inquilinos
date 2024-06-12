c79bb88fd94f9910bdd2299c526f5f22d45c9663
Valery Fonseca <117495689+valfonseca@users.noreply.github.com>
2024-06-12 13:59:52 -0600

    Visualizar solicitudes de mantenimiento

--- src/pages/propietario/MantenimientoP.js
+++ src/pages/propietario/MantenimientoP.js
@@ -4,24 +4,35 @@ import '../../css/Propiedades.css';
 
 function MantenimientoP(props) {
   const { isLogin, setIsLogin } = props;
+  const [editMode, setEditMode] = useState(false);
+  const [editedRow, setEditedRow] = useState(null);
+  const [properties, setProperties] = useState([]);
 
-  // Si nadie ha iniciado sesion lo envia a la ventana de login
+  const [editProperty, setEditProperties] = useState({
+    cedula: (localStorage.getItem('user')),
+    idSolicitud: "",
+    idPropiedad: "",
+    descripcion: "",
+    fechaSolicitud: "",
+    estado: "",
+    prioridad: "",
+    idProveedor: "",
+    nombreProveedor: "",
+    apellido1Proveedor: "",
+    apellido2Proveedor: "",
+    especialidad: "",
+    telefonoProveedor: ""
+  });
+
   if (!isLogin) {
     window.location.href = '/login';
   }
 
-  const [properties, setProperties] = useState([]);
-  const [editMode, setEditMode] = useState(null);
-  const [updatedProperties, setUpdatedProperties] = useState([]);
-
   useEffect(() => {
     if (isLogin) {
-      axios.post('http://localhost:8080/visualizarMantenimientos', {
-        cedula: 'cedulaPropietario' // Reemplaza esto con la variable que contiene la cédula del propietario
-      })
-      .then(response => {
-        setProperties(response.data);
-        setUpdatedProperties(response.data);
+      axios.post('http://localhost:8080/visualizarMantenimientos', { cedula: localStorage.getItem('user') })
+      .then((response) => {
+        setProperties(response.data.recordset);
       })
       .catch(error => {
         console.error("Error:", error);
@@ -29,109 +40,120 @@ function MantenimientoP(props) {
     }
   }, [isLogin]);
 
-  const handleEditClick = (index) => {
-    setEditMode(index);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setEditProperties((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
   };
 
-  const handleSaveClick = (index) => {
-    const updated = [...properties];
-    updated[index] = updatedProperties[index];
-    setProperties(updated);
-    setEditMode(null);
+  const toggleEditMode = () => {
+    setEditMode(!editMode);
+  };
 
-    const updatedProperty = updatedProperties[index];
-    axios.post('http://localhost:8080/actualizarMantenimientos', {
-      idSolicitud: updatedProperty.idSolicitud,
-      estado: updatedProperty.estado
-    })
-    .then(response => {
-      if (response.data.eliminarAmenidad) {
-        alert("Solicitud de mantenimiento actualizada exitosamente");
-      } else {
-        alert("Error al actualizar la solicitud de mantenimiento");
-      }
-    })
-    .catch(error => {
-      console.error("Error:", error);
-      alert("Error al conectar con el servidor");
+  const handleEdit = (rowIndex, property) => {
+    setEditProperties({
+      cedula: localStorage.getItem('user'),
+      idSolicitud: property.idSolicitud,
+      idPropiedad: property.idPropiedad,
+      descripcion: property.descripcion,
+      fechaSolicitud: property.fechaSolicitud,
+      estado: property.estado,
+      prioridad: property.prioridad,
+      idProveedor: property.idProveedor,
+      nombreProveedor: property.nombreProveedor,
+      apellido1Proveedor: property.apellido1Proveedor,
+      apellido2Proveedor: property.apellido2Proveedor,
+      especialidad: property.especialidad,
+      telefonoProveedor: property.telefonoProveedor
     });
+    setEditedRow(rowIndex);
+    toggleEditMode();
   };
 
-  const handleChange = (index, field, value) => {
-    const updated = [...updatedProperties];
-    updated[index][field] = value;
-    setUpdatedProperties(updated);
+  const handleSave = () => {
+    axios.post('http://localhost:8080/actualizarMantenimientos', editProperty)
+      .then((response) => {
+        if (response.data.actualizarMantenimiento) {
+          setEditedRow(null);
+          toggleEditMode();
+          axios.post('http://localhost:8080/visualizarMantenimientos', {
+            cedula: localStorage.getItem('user')
+          })
+          .then((response) => {
+            setProperties(response.data.recordset);
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
+        } else {
+          alert("Error al actualizar la solicitud de mantenimiento");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        alert("Error al conectar con el servidor");
+      });
   };
 
-  return (
-    <div className="propiedades">
-      <h1 className="title">Solicitudes de mantenimiento</h1>
-      <div className="table-contenedor">
-        <table className="table">
-          <thead>
-            <tr>
-              <th>ID Solicitud</th>
-              <th>ID Propiedad</th>
-              <th>Descripción</th>
-              <th>Fecha de solicitud</th>
-              <th>Estado</th>
-              <th>Prioridad</th>
-              <th>ID Proveedor</th>
-              <th>Nombre Proveedor</th>
-              <th>Apellido 1 Proveedor</th>
-              <th>Apellido 2 Proveedor</th>
-              <th>Especialidad</th>
-              <th>Télefono Proveedor</th>
-              <th>Acciones</th>
-            </tr>
-          </thead>
-          <tbody>
-            {properties.map((property, index) => (
-              <tr key={index}>
-                {editMode === index ? (
-                  <>
-                    <td><input type="number" value={updatedProperties[index].idSolicitud} onChange={(e) => handleChange(index, 'idSolicitud', e.target.value)} /></td>
-                    <td><input type="number" value={updatedProperties[index].idPropiedad} onChange={(e) => handleChange(index, 'idPropiedad', e.target.value)} /></td>
-                    <td><input type="text" value={updatedProperties[index].descripcion} onChange={(e) => handleChange(index, 'descripcion', e.target.value)} /></td>
-                    <td><input type="text" value={updatedProperties[index].fechaSolicitud} onChange={(e) => handleChange(index, 'fechaSolicitud', e.target.value)} /></td>
-                    <td><input type="number" value={updatedProperties[index].estado} onChange={(e) => handleChange(index, 'estado', e.target.value)} /></td>
-                    <td><input type="text" value={updatedProperties[index].prioridad} onChange={(e) => handleChange(index, 'prioridad', e.target.value)} /></td>
-                    <td><input type="number" value={updatedProperties[index].idProveedor} onChange={(e) => handleChange(index, 'idProveedor', e.target.value)} /></td>
-                    <td><input type="text" value={updatedProperties[index].nombreProveedor} onChange={(e) => handleChange(index, 'nombreProveedor', e.target.value)} /></td>
-                    <td><input type="text" value={updatedProperties[index].apellido1Proveedor} onChange={(e) => handleChange(index, 'apellido1Proveedor', e.target.value)} /></td>
-                    <td><input type="text" value={updatedProperties[index].apellido2Proveedor} onChange={(e) => handleChange(index, 'apellido2Proveedor', e.target.value)} /></td>
-                    <td><input type="text" value={updatedProperties[index].especialidad} onChange={(e) => handleChange(index, 'especialidad', e.target.value)} /></td>
-                    <td><input type="text" value={updatedProperties[index].telefonoProveedor} onChange={(e) => handleChange(index, 'telefonoProveedor', e.target.value)} /></td>
-                  </>
-                ) : (
-                  <>
-                    <td>{property.idSolicitud}</td>
-                    <td>{property.idPropiedad}</td>
-                    <td>{property.descripcion}</td>
-                    <td>{property.fechaSolicitud}</td>
-                    <td>{property.estado}</td>
-                    <td>{property.prioridad}</td>
-                    <td>{property.idProveedor}</td>
-                    <td>{property.nombreProveedor}</td>
-                    <td>{property.apellido1Proveedor}</td>
-                    <td>{property.apellido2Proveedor}</td>
-                    <td>{property.especialidad}</td>
-                    <td>{property.telefonoProveedor}</td>
-                  </>
-                )}
-                <td>
-                  {editMode === index ? (
-                    <a href="#" onClick={() => handleSaveClick(index)}>Guardar</a>
-                  ) : (
-                    <a href="#" onClick={() => handleEditClick(index)}>Actualizar</a>
-                  )}
-                </td>
+  if (isLogin) {
+    return (
+      <div className="propiedades">
+        <h1 className="title">Solicitudes de mantenimiento</h1>
+        <div className="table-contenedor">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>ID Solicitud</th>
+                <th>ID Propiedad</th>
+                <th>Descripción</th>
+                <th>Fecha de solicitud</th>
+                <th>Estado</th>
+                <th>Prioridad</th>
+                <th>ID Proveedor</th>
+                <th>Nombre Proveedor</th>
+                <th>Apellido 1 Proveedor</th>
+                <th>Apellido 2 Proveedor</th>
+                <th>Especialidad</th>
+                <th>Teléfono Proveedor</th>
+                <th>Acciones</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {properties.map((property, index) => (
+                <tr key={index}>
+                  <td>{editMode && editedRow === index ? <input type="text" defaultValue={editProperty.idSolicitud} name="idSolicitud" onChange={handleChange} /> : property.idSolicitud}</td>
+                  <td>{editMode && editedRow === index ? <input type="text" defaultValue={editProperty.idPropiedad} name="idPropiedad" onChange={handleChange} /> : property.idPropiedad}</td>
+                  <td>{editMode && editedRow === index ? <input type="text" defaultValue={editProperty.descripcion} name="descripcion" onChange={handleChange} /> : property.descripcion}</td>
+                  <td>{editMode && editedRow === index ? <input type="text" defaultValue={editProperty.fechaSolicitud} name="fechaSolicitud" onChange={handleChange} /> : property.fechaSolicitud}</td>
+                  <td>{editMode && editedRow === index ? <input type="text" defaultValue={editProperty.estado} name="estado" onChange={handleChange} /> : property.estado}</td>
+                  <td>{editMode && editedRow === index ? <input type="text" defaultValue={editProperty.prioridad} name="prioridad" onChange={handleChange} /> : property.prioridad}</td>
+                  <td>{editMode && editedRow === index ? <input type="text" defaultValue={editProperty.idProveedor} name="idProveedor" onChange={handleChange} /> : property.idProveedor}</td>
+                  <td>{editMode && editedRow === index ? <input type="text" defaultValue={editProperty.nombreProveedor} name="nombreProveedor" onChange={handleChange} /> : property.nombreProveedor}</td>
+                  <td>{editMode && editedRow === index ? <input type="text" defaultValue={editProperty.apellido1Proveedor} name="apellido1Proveedor" onChange={handleChange} /> : property.apellido1Proveedor}</td>
+                  <td>{editMode && editedRow === index ? <input type="text" defaultValue={editProperty.apellido2Proveedor} name="apellido2Proveedor" onChange={handleChange} /> : property.apellido2Proveedor}</td>
+                  <td>{editMode && editedRow === index ? <input type="text" defaultValue={editProperty.especialidad} name="especialidad" onChange={handleChange} /> : property.especialidad}</td>
+                  <td>{editMode && editedRow === index ? <input type="text" defaultValue={editProperty.telefonoProveedor} name="telefonoProveedor" onChange={handleChange} /> : property.telefonoProveedor}</td>
+                  <td>
+                    {editMode && editedRow === index ? (
+                      <a href="#" onClick={() => handleSave()}>Guardar</a>
+                    ) : (
+                      <span>
+                        <a href="#" onClick={() => handleEdit(index, property)}>Editar</a>
+                      </span>
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
+    );
+  }
+  return (
+    <div className="home"></div>
   );
 }
 
