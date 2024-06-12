2a4f096fa0e6c14b317ce370cce2c00a36ca28c3
nicoArley <nikyac13@gmail.com>
2024-06-12 17:26:15 -0600

    visualizar alquileres activos

--- src/pages/Inquilino/VisualizarAlquileres.js
+++ src/pages/Inquilino/VisualizarAlquileres.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import '../../css/Propiedades.css';
 
 const getFecha = (fecha) => {
@@ -8,52 +9,84 @@ const getFecha = (fecha) => {
 
 function VisualizarAlq(props) {
   const { isLogin } = props;
-  const [editMode, setEditMode] = useState(false);
-  const [editedRow, setEditedRow] = useState(null);
-  const [amenidades, setAmenidades] = useState([
-    { id: 1, costo: 100, tipo: "Gimnasio", descripción: "Gimnasio completamente equipado", estado: 1 },
-    { id: 2, costo: 50, tipo: "Piscina", descripción: "Piscina climatizada", estado: 2 }
-  ]);
+  const cedula = localStorage.getItem('user');
+  const [alquileresP, setAlquileresP] = useState([]);
+  const [alquileresA, setAlquileresA] = useState([]);
+  if (!isLogin) {
+    window.location.href = '/login';
+  }
+  useEffect(() => {
+    if (isLogin) {
+      // Reemplaza "cedulaDelUsuario" con la cédula del usuario logueado
+      axios.post('http://localhost:8080/visualizarAlquilerIP', { cedula })
+        .then(response => {
+          setAlquileresP(response.data.recordset);
+        })
+        .catch(error => {
+          console.error("Error fetching alquileresP:", error);
+        });
 
-  const toggleEditMode = () => {
-    setEditMode(!editMode);
-  };
-
-  const handleEdit = (rowIndex) => {
-    setEditedRow(rowIndex);
-    toggleEditMode();
-  };
-
-  const handleSave = () => {
-    // Guardar los cambios
-    setEditedRow(null);
-    toggleEditMode();
-  };
-
-  return (
+        axios.post('http://localhost:8080/visualizarAlquilerIA', { cedula })
+        .then(response => {
+          setAlquileresA(response.data.recordset);
+        })
+        .catch(error => {
+          console.error("Error fetching alquileresP:", error);
+        });
+    }
+  }, [isLogin]);
+  
+  if(isLogin){ 
+    return (
     <div className="propiedades">
-      <h1 className="title">Visualizar alquileres</h1>
+      <h1 className="title">Visualizar alquileres activos </h1>
+      <h2 className="subtitle">Alquileres de propiedades</h2>
       <table className="table">
         <thead>
           <tr>
-            <th>ID alquiler</th>
-            <th>fecha Inicio</th>
+            <th>Id propiedad</th>
+            <th>Direccion</th>
+            <th>Fecha Inicio</th>
             <th>Fecha Fin</th>
           </tr>
         </thead>
         <tbody>
-          {amenidades.map((amenidad, index) => (
+          {alquileresP?.map(function (alquiler, index) {
+            return (
             <tr key={index}>
-              <td>{amenidad.id}</td>
-              <td>{editMode && editedRow === index ? <input type="number" defaultValue={amenidad.fechaInicio} /> : amenidad.fechaFin}</td>
-              <td>
-              </td>
-            </tr>
-          ))}
+              <td>{alquiler.idPropiedad}</td>
+              <td>{alquiler.direccion}</td>
+              <td>{getFecha(new Date (alquiler.fechaInicio))}</td>
+              <td>{getFecha(new Date (alquiler.fechaFin ))}</td>
+            </tr>)
+          })}
+        </tbody>
+      </table>
+      <br/><br/> 
+      <h2 className="subtitle">Alquileres de amenidad</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Id amenidad</th> 
+            <th>Tipo</th>
+            <th>Fecha Inicio</th>
+            <th>Fecha Fin</th>
+          </tr>
+        </thead>
+        <tbody>
+          {alquileresA?.map(function (alquiler, index) {
+            return (
+            <tr key={index}>
+              <td>{alquiler.idAmenidad}</td>
+              <td>{alquiler.tipoAmenidad}</td>
+              <td>{getFecha(new Date (alquiler.fechaInicio))}</td>
+              <td>{getFecha(new Date (alquiler.fechaFin ))}</td>
+            </tr>)
+          })}
         </tbody>
       </table>
     </div>
   );
 }
-
+}
 export default VisualizarAlq;
